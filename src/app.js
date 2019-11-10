6b2b8b78db42bb4c03c1e8e1b1160a357fafe698
Max Stevenson <maxstevenson@msn.com>
2019-11-10 21:36:35 +0000

    react router installed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import '../styles/styles.scss';
 
-ReactDOM.render(<p>This is boilerplate</p>, document.getElementById('app'));
+ReactDOM.render(<p>This is a Budget App</p>, document.getElementById('app'));
