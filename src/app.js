b06952fec08890533a940e40c10c925c0bfc1dd8
Max Stevenson <maxstevenson@msn.com>
2019-11-11 20:42:11 +0000

    Links setup to pages in app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import '../styles/styles.scss';
 
@@ -21,18 +21,33 @@ const HelpPage = () => (
 );
 
 const NotFoundPage = () => (
-	<div>404</div>
+	<div>
+		404 - <Link to="/">Go Home</Link>
+	</div>
 );
+
+const Header = () => (
+	<header>
+		<h1>Expense App</h1>
+		<Link to="/">Home</Link>
+		<Link to="/create">Add Expense</Link>
+		<Link to="/edit">Edit Expense</Link>
+		<Link to="/help">Help Page</Link>
+	</header>
+)
 
 const routes = (
 	<BrowserRouter>
-		<Switch>
-			<Route path="/" component={ExpenseDashboardPage} exact={true} />
-			<Route path="/create" component={AddExpensePage} />
-			<Route path="/edit" component={EditExpensePage} />
-			<Route path="/help" component={HelpPage} />
-			<Route component={NotFoundPage} />
-		</Switch>
+		<div>
+			<Header />
+			<Switch>
+				<Route path="/" component={ExpenseDashboardPage} exact={true} />
+				<Route path="/create" component={AddExpensePage} />
+				<Route path="/edit" component={EditExpensePage} />
+				<Route path="/help" component={HelpPage} />
+				<Route component={NotFoundPage} />
+			</Switch>
+		</div>
 	</BrowserRouter>
 );
 
