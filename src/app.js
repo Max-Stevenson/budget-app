8b673ccd4bc5fe9607b12f02c49850212845eae5
Max Stevenson <maxstevenson@msn.com>
2019-11-11 07:18:14 +0000

    More routes added - help page component - edit expense component

--- src/app.js
+++ src/app.js
@@ -12,11 +12,21 @@ const AddExpensePage = () => (
     <div>Expense page</div>
 );
 
+const EditExpensePage = () => (
+    <div>Edit Expense page</div>
+);
+
+const HelpPage = () => (
+    <div>Help page</div>
+);
+
 const routes = (
     <BrowserRouter>
         <div>
         <Route path="/" component={ExpenseDashboardPage} exact={true}/>
         <Route path="/create" component={AddExpensePage}/>
+        <Route path="/edit" component={EditExpensePage}/>
+        <Route path="/help" component={HelpPage}/>
         </div>
     </BrowserRouter>
 );
