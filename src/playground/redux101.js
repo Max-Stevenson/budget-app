61891147e2fa23d9cf5f1388dc37e1342fcedb18
Max Stevenson <maxstevenson@msn.com>
2019-11-14 20:02:22 +0000

    More fun with redux

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,7 +1,19 @@
-import {createStore} from 'redux';
+import {createStore, bindActionCreators} from 'redux';
 
-const store = createStore((state = {count: 0}) => {
+const store = createStore((state = {count: 0}, action) => {
+  if (action.type === 'INCREMENT') {
+    return {
+      count: state.count + 1
+    };
+  };
   return state;
 });
 
-store.getState();
+store.dispatch({
+  type: 'INCREMENT'
+});
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+console.log(store.getState());
