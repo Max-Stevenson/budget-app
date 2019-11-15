dd6aa270c5c4508fc538427b6331242cf071620a
Max Stevenson <maxstevenson@msn.com>
2019-11-15 11:22:38 +0000

    Refactor to use functions in dispatch calls

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,16 +1,24 @@
 import {createStore, bindActionCreators} from 'redux';
 
+const incrementCount = (payload = {}) => ({
+  type: 'INCREMENT',
+  incrementBy: typeof payload.incrementBy === 'number' ? payload.incrementBy : 1
+});
+
+const decrementCount = (payload = {}) => ({
+  type: 'DECREMENT',
+  decrementBy: typeof payload.decrementBy === 'number' ? payload.decrementBy : 1
+});
+
 const store = createStore((state = {count: 0}, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
     case 'SET':
       return {
@@ -29,17 +37,9 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
-store.dispatch({
-  type: 'INCREMENT'
-});
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 2
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
+store.dispatch(decrementCount({ decrementBy: 2 }));
+
 store.dispatch({
   type: 'SET',
   count: 100
