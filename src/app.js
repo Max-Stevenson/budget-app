088c024a23624c986d38ae855be234dd4dd118a5
Max Stevenson <maxstevenson@msn.com>
2019-11-17 10:39:03 +0000

    Setup complete for individual expense rendering

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ import 'normalize.css/normalize.css';
 import '../styles/styles.scss';
 
 const store = configureStore();
-store.dispatch(addExpense({description: 'water Bill'}));
-store.dispatch(addExpense({description: 'gas Bill'}));
-store.dispatch(setTextFilter('gas'));
+store.dispatch(addExpense({description: 'water Bill', amount: 4500}));
+store.dispatch(addExpense({description: 'gas Bill', amount: 6500}));
+store.dispatch(setTextFilter('Bill'));
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
