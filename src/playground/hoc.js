49b19e5d414646766193c9498e0a5376e6ac6aad
Max Stevenson <maxstevenson@msn.com>
2019-11-17 10:00:00 +0000

    Practicing with higher order components

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -3,9 +3,31 @@ import ReactDOM from 'react-dom';
 
 const Info = (props) => (
   <div>
-    <h1>Infor</h1>
+    <h1>Info</h1>
     <p>Some Information: {props.info}</p>
   </div>
 );
 
-ReactDOM.render(<Info info='some details'/>, document.getElementById('app'));
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAdmin && <p>This is Official Sensitive</p>}
+      <WrappedComponent {...props}/>
+    </div>
+  );
+};
+
+const AdminInfo = withAdminWarning(Info);
+
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <div>
+    {props.isAuthenticated && <p>User is authenticated</p>}
+    <WrappedComponent {...props} />
+    </div>
+  );
+};
+
+const AuthInfo = requireAuthentication(Info)
+
+ReactDOM.render(<AuthInfo isAuthenticated={true} info='some details'/>, document.getElementById('app'));
