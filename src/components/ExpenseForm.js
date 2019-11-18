8fef1194651b872d057a91cec13b74729153a2b6
Max Stevenson <maxstevenson@msn.com>
2019-11-18 15:03:03 +0000

    Expense form WIP
    - regex changed to allow for deletion.
    - prevented user from deleting dates.
    - adding expenses now stores them in redux.
    - basic validation for expense form submission.

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -11,7 +11,8 @@ export default class ExpenseForm extends React.Component {
     note: '',
     amount: '',
     createdAt: moment(),
-    calandarFocused: false
+    calandarFocused: false,
+    error: ''
   };
 
   onDescriptionChange = (e) => {
@@ -26,23 +27,42 @@ export default class ExpenseForm extends React.Component {
 
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({amount}));
     };
   };
 
   onDateChange = (createdAt) => {
-    this.setState(() => ({ createdAt }));
+    if (createdAt) {
+      this.setState(() => ({ createdAt }));
+    };
   };
 
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calandarFocused: focused }));
   };
 
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!this.state.description || !this.state.amount) {
+      this.setState(() => ({ error: 'Please enter a description and amount' }));
+    } else {
+      this.setState(() => ({ error: '' }));
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
+    };
+  };
+
   render() {
     return (
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
           type="text"
           placeholder="Description"
@@ -62,6 +82,7 @@ export default class ExpenseForm extends React.Component {
           focused={this.state.calandarFocused}
           onFocusChange={this.onFocusChange}
           numberOfMonths={1}
+          isOutsideRange={(day) => {false}}
           />
           <textarea
           placeholder="Add a note for your expense (optional)"
