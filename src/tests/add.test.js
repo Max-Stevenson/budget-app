3b2f4c242690f93fd7a033d16060c4a9af96d915
Max Stevenson <maxstevenson@msn.com>
2019-11-19 19:09:00 +0000

    Change jest script to watch for changes

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,6 +1,13 @@
 const add = (a, b) => a + b;
 
-test('should add two numbers', () => {
+const generateGreeting = (name) => `Hello ${name}`;
+
+test('Should add two numbers', () => {
   const result = add(1,4);
   expect(result).toBe(5);
-})
+});
+
+test('Should output name in greeting', () => {
+  const result = generateGreeting('test');
+  expect(result).toMatch('Hello test');
+});
