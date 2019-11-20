510c6675203819bd60cccb420938634d4ce6863e
Max Stevenson <maxstevenson@msn.com>
2019-11-20 21:19:00 +0000

    Full test coverage for expense action generators.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,7 +1,8 @@
-import {addExpense, editExpense, removeExpense} from '../../actions/expenses';
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import { string } from 'postcss-selector-parser';
 
 test('Should setup remove expense action object', () => {
-  const action = removeExpense({id: 'test'});
+  const action = removeExpense({ id: 'test' });
   expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
     id: 'test'
@@ -15,6 +16,37 @@ test('Should setup edit expense action object', () => {
     id: 'test',
     updates: {
       note: 'test'
+    }
+  });
+});
+
+test('Should setup add expense action object', () => {
+  const expenseData = {
+    description: 'testDescription',
+    note: 'testNote',
+    amount: 1,
+    createdAt: 1
+  };
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseData,
+      id: expect.any(String)
+    }
+  });
+});
+
+test('Should setup add expense action object with default values', () => {
+  const action = addExpense();
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      description: '',
+      note: '',
+      amount: 0,
+      createdAt: 0,
+      id: expect.any(String)
     }
   });
 });
