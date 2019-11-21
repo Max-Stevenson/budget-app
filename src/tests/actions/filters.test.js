6b68d50bfd7bd0ce9366ac729491041a6f8c0c13
Max Stevenson <maxstevenson@msn.com>
2019-11-21 19:08:58 +0000

    Finished tests for filters action objects.

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -18,5 +18,31 @@ test('Should generate set end date action object', () => {
 });
 
 test('Should generate sort by amount action object', () => {
+  const action = sortByAmount();
+  expect(action).toEqual({
+    type: 'SORT_BY_AMOUNT'
+  });
+});
 
+test('Should generate set text filter object with text value', () => {
+  const action = setTextFilter('test');
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: 'test'
+  });
+});
+
+test('Should generate set text filter object with default value', () => {
+  const action = setTextFilter();
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: ''
+  });
+});
+
+test('Should generate sort by date filter object', () => {
+  const action = sortByDate();
+  expect(action).toEqual({
+    type: 'SORT_BY_DATE'
+  });
 });
