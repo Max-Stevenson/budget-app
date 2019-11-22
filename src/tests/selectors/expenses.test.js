9df8535a168a5e9ac8efa2ff227e57f2ebdda3e1
Max Stevenson <maxstevenson@msn.com>
2019-11-22 14:16:35 +0000

    All tests laid out for selectorExpenses.

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,4 +1,5 @@
 import selectExpenses from '../../selectors/expenses';
+import moment from 'moment';
 
 const expenses = [{
   id: '1',
@@ -11,13 +12,13 @@ const expenses = [{
   description: 'test two',
   note: 'test',
   amount: 1,
-  createdAt: -1000
+  createdAt: moment(0).subtract(4, 'days').valueOf()
 }, {
   id: '3',
   description: 'test three',
   note: 'test',
   amount: 2,
-  createdAt: 1000
+  createdAt: moment(0).add(4, 'days').valueOf()
 }];
 
 test('Should filter by text value', () => {
@@ -31,6 +32,25 @@ test('Should filter by text value', () => {
   expect(result).toEqual([expenses[1]]);
 });
 
-test('Should filter by start date', () => {
-  
-})
+test('Should filter by startDate', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: moment(0),
+    endDate: undefined
+  }
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[0]])
+});
+
+test('Should sort by amount', () => {
+
+});
+
+test('Should sort by date', () => {
+
+});
+
+test('Should sort by endDate', () => {
+
+});
