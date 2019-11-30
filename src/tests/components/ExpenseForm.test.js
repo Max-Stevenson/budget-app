a57276f84c2616b71bbc8977ee4a485d37a11171
Max Stevenson <maxstevenson@msn.com>
2019-11-30 10:40:45 +0000

    WIP for tests simulating user interaction with ExpenseForm.

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,11 @@ test('Should render ExpenseForm correctly with expense data', () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('Should render error for invalid form submission', () => {
+  const wrapper = shallow(<ExpenseForm/>);
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => { }
+  });
+
+});
