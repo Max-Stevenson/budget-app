20723deac9d669301ed7c48c7504c7651a927245
Max Stevenson <maxstevenson@msn.com>
2019-12-02 20:24:35 +0000

    BeforeEach hook added to test file.

--- src/tests/components/AddExpense.test.js
+++ src/tests/components/AddExpense.test.js
@@ -3,19 +3,19 @@ import { shallow } from 'enzyme';
 import { AddExpensePage } from '../../components/AddExpensePage';
 import expenses from '../fixtures/expenses';
 
+let onSubmit, history, wrapper;
+
+beforeEach(() => {
+  onSubmit = jest.fn();
+  history = { push: jest.fn() };
+  wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+});
+
 test('Should render AddExpensePage correctly', () => {
-  const onSubmit = jest.fn();
-  const history = { push: jest.fn() };
-  const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history}/>);
-  
   expect(wrapper).toMatchSnapshot();
 });
 
 test('Should handle onSubmit', () => {
-  const onSubmit = jest.fn();
-  const history = { push: jest.fn() };
-  const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history}/>);
-
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[0]);
   expect(history.push).toHaveBeenLastCalledWith('/');
   expect(onSubmit).toHaveBeenLastCalledWith(expenses[0]);
