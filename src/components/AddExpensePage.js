37e7093fb9cea66eb7c69ab0064460b8a4b3c900
Max Stevenson <maxstevenson@msn.com>
2019-12-02 20:06:23 +0000

    Refactor of AddExpense
    - Used mapDispatchToProps to simplify for testing.
    - Test file created for AddExpense comp.

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -8,11 +8,17 @@ const AddExpensePage = (props) => (
 		<h1>Add Expense</h1>
 		<ExpenseForm
 		onSubmit={(expense) => {
-			props.dispatch(addExpense(expense));
+			props.onSubmit(expense);
 			props.history.push('/');
 		}}
 		/>
 	</div>
 );
 
-export default connect()(AddExpensePage);
+const mapDispatchToProps = () => {
+	return {
+		onSubmit: (expense) => dispatch(addExpense(expense))
+	};
+};
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
