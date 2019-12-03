06324c4df69545711b48250e63f6cfe56449b95a
Max Stevenson <maxstevenson@msn.com>
2019-12-03 16:57:20 +0000

    All tests created for EditExpensePage.

--- src/tests/components/EditExpense.test.js
+++ src/tests/components/EditExpense.test.js
@@ -21,10 +21,14 @@ test('Should render EditExpensePage', () => {
   expect(wrapper).toMatchSnapshot();
 });
 
-test('', () => {
-
+test('should handle editExpense', () => {
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[0]);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(editExpense).toHaveBeenLastCalledWith(expenses[0].id, expenses[0]);
 });
 
-test('', () => {
-
+test('Should handle removeExpense', () => {
+  wrapper.find('button').simulate('click');
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(removeExpense).toHaveBeenLastCalledWith({id: expenses[0].id});
 });
