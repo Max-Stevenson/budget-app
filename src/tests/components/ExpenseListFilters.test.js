e3f9dd4cbf32391df5ce61f2e9c7e96eb22cf76a
Max Stevenson <maxstevenson@msn.com>
2019-12-07 13:30:25 +0000

    First test created for ExpenseListFilters

---
+++ src/tests/components/ExpenseListFilters.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExpenseListFilters } from '../../components/ExpenseListFilters';
+import { filters, altFilters } from '../fixtures/filters';
+
+let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
+
+beforeEach(() => {
+  setTextFilter = jest.fn();
+  sortByAmount = jest.fn();
+  sortByDate = jest.fn();
+  setStartDate = jest.fn();
+  setEndDate = jest.fn();
+  wrapper = shallow(
+    <ExpenseListFilters
+    filters={filters}
+    setTextFilter={setTextFilter}
+    sortByAmount={sortByAmount}
+    sortByDate={sortByDate}
+    setStartDate={setStartDate}
+    setEndDate={setEndDate}
+    />)
+});
+
+test('Should render correctly with default filters', () => {
+  expect(wrapper).toMatchSnapshot();
+});
