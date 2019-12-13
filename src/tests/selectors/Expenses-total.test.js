0704e139cd3401ce458ab90f1b2d36eda67cf60c
Max Stevenson <maxstevenson@msn.com>
2019-12-13 12:50:18 +0000

    All tests written for new selector.

--- src/tests/selectors/Expenses-total.test.js
+++ src/tests/selectors/Expenses-total.test.js
@@ -5,3 +5,13 @@ test('Should return 0 if no expenses', () => {
   const res = selectExpensesTotal([]);
   expect(res).toBe(0);
 });
+
+test('Should correctly add up a single expense', () => {
+  const res = selectExpensesTotal([expenses[0]]);
+  expect(res).toBe(1);
+});
+
+test('Should correctly add up multiple expenses', () => {
+  const res = selectExpensesTotal();
+  expect(res).toBe(5);
+});
