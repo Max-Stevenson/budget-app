73e16dc07d787c3d9f158ef0e712325ec87ef3be
Max Stevenson <maxstevenson@msn.com>
2020-01-05 14:00:30 +0000

    test case written for editting firebase expenses.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
   removeExpense, 
   setExpenses, 
   startSetExpenses, 
-  startRemoveExpense } from '../../actions/expenses';
+  startRemoveExpense, 
+  startEditExpense} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -53,6 +54,27 @@ test('Should setup edit expense action object', () => {
     updates: {
       note: 'test'
     }
+  });
+});
+
+test('Should edit expense from firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = {
+    amount: 2000
+  };
+  
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id,
+      updates
+    });
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val().amount).toBe(updates.amount);
+    done();
   });
 });
 
