099cd93da83baf5e16e3964059f8a9297cdaa138
Max Stevenson <maxstevenson@msn.com>
2020-01-05 17:26:39 +0000

    Tests created for login and logout functionality.

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -7,3 +7,11 @@ test('Should render Header correctly', () => {
   const wrapper = shallow(<Header startLogout={() => {}}/>);
   expect(toJSON(wrapper)).toMatchSnapshot();
 });
+
+test('Should call startLogout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout}/>);
+  
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
+});
--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -6,3 +6,11 @@ test('Should correctly render LoginPage correctly', () => {
   const wrapper = shallow(<LoginPage/>);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('Should call startLogin on button click', () => {
+  const startLogin = jest.fn();
+  const wrapper = shallow(<LoginPage startLogin={startLogin}/>);
+  
+  wrapper.find('button').simulate('click');
+  expect(startLogin).toHaveBeenCalled();
+});
