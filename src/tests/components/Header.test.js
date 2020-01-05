954eeef849521b58e2bef5c440e035085126d9ac
Max Stevenson <maxstevenson@msn.com>
2020-01-05 17:18:14 +0000

    Logout button added to Header
    - Updated snapshot tests for header.

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import Header from '../../components/Header';
+import {Header} from '../../components/Header';
 
 test('Should render Header correctly', () => {
-  const wrapper = shallow(<Header/>);
+  const wrapper = shallow(<Header startLogout={() => {}}/>);
   expect(toJSON(wrapper)).toMatchSnapshot();
 });
