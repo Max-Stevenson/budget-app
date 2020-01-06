da1244e25e1ee4639045f31b208616527c132cd4
Max Stevenson <maxstevenson@msn.com>
2020-01-06 19:43:29 +0000

    auth reducer implemented in store.
    - updated login and logout functionality to store auth state in redux.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import AppRouter, { history } from '../src/routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
 import { Provider } from 'react-redux';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
@@ -31,6 +31,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
+    store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
@@ -38,6 +39,7 @@ firebase.auth().onAuthStateChanged((user) => {
       };
     });
   } else {
+    store.dispatch(logout());
     renderApp();
     history.push('/');
   };
