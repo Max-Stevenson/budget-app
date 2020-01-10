163cb5de737fdb803178f996e144553680482d65
Max Stevenson <maxstevenson@msn.com>
2020-01-10 10:17:11 +0000

    Styling button on LoginPage.

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,7 +7,7 @@ export const LoginPage = ({ startLogin }) => (
     <div className="box-layout__box">
       <h1 className="box-layout__title">Budget App</h1>
       <p>Manage your budget!</p>
-      <button onClick={startLogin}>Login</button>
+      <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
 );
